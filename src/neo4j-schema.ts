fef6df17a9a5334a8cfb1ea54751b678c1bd5e0b
Kyle Qin <43757365+kylezehaoqin@users.noreply.github.com>
2023-04-19 20:44:43 -0400

    enable project-people match

--- src/neo4j-schema.ts
+++ src/neo4j-schema.ts
@@ -29,8 +29,9 @@ export const typeDefs = gql`
     id: ID @id
     name: String! @unique
     useSkills: [Skill!]! @relationship(type: "USES_SKILL", direction: OUT)
+    matchPeople: [PeopleMatchResult]!
   }
-  
+
   input SkillInput {
     name: String!
     description: String
@@ -54,9 +55,12 @@ export const typeDefs = gql`
     similar_missed_skills: [String]
   }
 
-#   type Query {
-#     matchProjects(personName: String!): [MatchResult] 
-#   }
+  type PeopleMatchResult {
+    people: Person
+    coverage_score: Float
+    missed_skills: [String]
+    similar_missed_skills: [String]
+  }
 
 `;
 
@@ -116,19 +120,61 @@ export const resolvers = {
         }
     },
 
-    // Project: {
-    //     useSkills: (project: any) => {
-    //         let session = context().driver.session();
-    //         let params = {name: project.name};
-    //         let query = `
-    //             MATCH (p: Project {name: $name})-[:USES_SKILL]->(s: Skill) 
-    //             RETURN s AS skills;
-    //         `
-    //         return session.run(query, params).then( 
-    //             result => {return result.records.map(record => {return record.get("skills")})}
-    //         )
-    //     }
-    // }
+    Project: {
+
+        matchPeople: (project: any) => {
+            let session = context().driver.session();
+            let params = {name: project.name};
+            let query = `
+            MATCH (p: Person)-[:HAS_SKILL]->(s:Skill)<-[:USES_SKILL]-(pr: Project)
+                WHERE toLower(pr.name) = toLower($name)
+            MATCH (pr)-[:USES_SKILL]->(req_skill:Skill)
+            MATCH (p)-[:HAS_SKILL]-(had_skills:Skill)
+            WITH 
+                p, 
+                pr, 
+                COLLECT(DISTINCT s.name) AS shared_skills, 
+                COLLECT(DISTINCT req_skill.name) AS required_skills, 
+                COLLECT(DISTINCT had_skills.name) AS had_skills
+            UNWIND had_skills AS hs
+            UNWIND required_skills as rs
+            UNWIND shared_skills as ss
+            MATCH (parent:Skill)<-[:HAS_PARENT_SKILL]-(child:Skill) WHERE (child.name IN [skill in required_skills WHERE NOT skill IN had_skills])
+            MATCH (parent)-[:CONTAINS_SKILL]->(sub_skill:Skill)
+            WHERE (NOT sub_skill.name IN ss) AND (sub_skill.name in hs) AND (NOT sub_skill.name IN rs)
+
+            WITH 
+                p, 
+                pr, 
+                required_skills, 
+                COLLECT(DISTINCT sub_skill.name) AS similar_missed_skills, 
+                had_skills, 
+                shared_skills, 
+                [skill in required_skills WHERE NOT skill IN had_skills] AS missed_skills
+            RETURN 
+                p AS people, 
+                pr AS project, 
+                round(toFloat(size(shared_skills) + size(similar_missed_skills)*0.5) / size(required_skills), 3) as coverage_score,
+                missed_skills,
+                similar_missed_skills
+            ORDER BY coverage_score DESC;
+            `
+            return session.run(
+                query, params
+            ).then (result => result.records.map(record => 
+                {
+                    return {
+                        project: record.get('project').properties,
+                        people: record.get('people').properties,
+                        missed_skills: record.get('missed_skills'),
+                        similar_missed_skills: record.get('similar_missed_skills'),
+                        coverage_score: record.get('coverage_score')
+                    }
+                })
+            )
+
+        }
+    },
 };
 
   
